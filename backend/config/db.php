7e089bc19175a1c518f46df1baf6f6bfbeab5fb0
DevWithShakil <m.shakilkhan720@gmail.com>
2025-08-18 11:25:31 +0600

    connect with database

---
+++ backend/config/db.php
@@ -0,0 +1,14 @@
+<?php 
+$host = "localhost";
+$port = "5432";
+$dbname = "hospital_appointment_management_system";
+$user = "postgres";
+$password = "12345";
+
+try{
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+?>
