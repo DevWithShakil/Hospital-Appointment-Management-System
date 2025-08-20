a4877472ea982a4abdd6494a888605ff086042e6
DevWithShakil <m.shakilkhan720@gmail.com>
2025-08-20 13:19:30 +0600

    update for deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hospital/appointment-management-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8cfc5ee82b2fdbc708ad7226f337ded58b14b8e0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hospital/appointment-management-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8cfc5ee82b2fdbc708ad7226f337ded58b14b8e0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
